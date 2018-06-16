e95b164855d83f3b14bdaef0ffcabcccbf2ffb68
saad helal <saad.helal.sh@gmail.com>
2018-06-16 16:54:44 +0200

    changed the database

--- cr09_saad_helal_carrental.sql
+++ cr09_saad_helal_carrental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2018 at 04:02 PM
+-- Generation Time: Jun 16, 2018 at 04:54 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -31,9 +31,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `car` (
   `car_ID` int(11) NOT NULL,
   `car_type` varchar(30) NOT NULL,
-  `car_model` varchar(30) NOT NULL,
-  `customer_ID` int(11) NOT NULL
+  `car_model` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `car`
+--
+
+INSERT INTO `car` (`car_ID`, `car_type`, `car_model`) VALUES
+(1, 'SUV', 'Jeep Cherokee'),
+(2, 'SUV', 'Audi Q7'),
+(3, 'Sport', 'Ferrari F40'),
+(4, 'Sport', 'Lamborghini Aventador'),
+(5, 'Economy', 'Citroën C1'),
+(6, 'Economy', 'Volkswagen Golf'),
+(7, 'Minivan', 'Toyota Sienna'),
+(8, 'Minivan', 'Chrysler Pacifica'),
+(9, 'Hatchback', 'Mazda3'),
+(10, 'Hatchback', 'Hyundai Accent');
 
 -- --------------------------------------------------------
 
@@ -43,10 +58,24 @@ CREATE TABLE `car` (
 
 CREATE TABLE `customer` (
   `customer_ID` int(11) NOT NULL,
-  `customer_name` varchar(30) NOT NULL,
-  `rent_ID` int(11) NOT NULL,
-  `car_ID` int(11) NOT NULL
+  `customer_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_ID`, `customer_name`) VALUES
+(1, 'saad Helal'),
+(2, 'Joesef Walker'),
+(3, 'Alex Hardo'),
+(4, 'John Dow'),
+(5, 'Jane Smith'),
+(6, 'Sara Berger'),
+(7, 'Andrea Roschek'),
+(8, 'Sam Ford'),
+(9, 'Yousef Khattab'),
+(10, 'Samer Ahmad');
 
 -- --------------------------------------------------------
 
@@ -59,8 +88,21 @@ CREATE TABLE `pick_up` (
   `start_location` varchar(30) NOT NULL,
   `date_of_renting` date NOT NULL,
   `invoice_number` int(11) NOT NULL,
-  `car_ID` int(11) NOT NULL
+  `FK_car_ID` int(11) NOT NULL,
+  `fk_customer_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pick_up`
+--
+
+INSERT INTO `pick_up` (`rent_ID`, `start_location`, `date_of_renting`, `invoice_number`, `FK_car_ID`, `fk_customer_ID`) VALUES
+(1, 'vienna-thaliastrasse', '2018-06-03', 111, 3, 3),
+(2, 'vienna-meidling', '2018-03-01', 123, 1, 1),
+(3, 'vienna-Johnstrasse', '2018-04-24', 111, 2, 2),
+(4, 'borgenland-eisenstadt', '2018-06-13', 112, 3, 5),
+(5, 'burgenland-rechnitz', '2018-06-21', 113, 8, 6),
+(6, 'viennakeisermülle', '2018-06-17', 114, 7, 7);
 
 -- --------------------------------------------------------
 
@@ -73,8 +115,16 @@ CREATE TABLE `return` (
   `additional_charges` int(11) NOT NULL,
   `date_of_returning` date NOT NULL,
   `return_location` varchar(30) NOT NULL,
-  `customer_ID` int(11) NOT NULL
+  `fk_customer_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `return`
+--
+
+INSERT INTO `return` (`return_ID`, `additional_charges`, `date_of_returning`, `return_location`, `fk_customer_ID`) VALUES
+(1, 0, '2018-06-15', 'vienna-meidling', 3),
+(2, 25, '2018-07-19', 'vienna-johnstrasse', 5);
 
 --
 -- Indexes for dumped tables
@@ -84,59 +134,73 @@ CREATE TABLE `return` (
 -- Indexes for table `car`
 --
 ALTER TABLE `car`
-  ADD PRIMARY KEY (`car_ID`),
-  ADD KEY `customer_ID` (`customer_ID`);
+  ADD PRIMARY KEY (`car_ID`);
 
 --
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_ID`),
-  ADD KEY `car_ID` (`car_ID`),
-  ADD KEY `rent_ID` (`rent_ID`);
+  ADD PRIMARY KEY (`customer_ID`);
 
 --
 -- Indexes for table `pick_up`
 --
 ALTER TABLE `pick_up`
   ADD PRIMARY KEY (`rent_ID`),
-  ADD KEY `car_ID` (`car_ID`);
+  ADD KEY `FK_car_ID` (`FK_car_ID`),
+  ADD KEY `fk_customer_ID` (`fk_customer_ID`);
 
 --
 -- Indexes for table `return`
 --
 ALTER TABLE `return`
   ADD PRIMARY KEY (`return_ID`),
-  ADD KEY `customer_ID` (`customer_ID`);
+  ADD KEY `customer_ID` (`fk_customer_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `car`
+--
+ALTER TABLE `car`
+  MODIFY `car_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `pick_up`
+--
+ALTER TABLE `pick_up`
+  MODIFY `rent_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `return`
+--
+ALTER TABLE `return`
+  MODIFY `return_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `car`
---
-ALTER TABLE `car`
-  ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`customer_ID`) REFERENCES `customer` (`customer_ID`);
-
---
--- Constraints for table `customer`
---
-ALTER TABLE `customer`
-  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`car_ID`) REFERENCES `car` (`car_ID`),
-  ADD CONSTRAINT `customer_ibfk_2` FOREIGN KEY (`rent_ID`) REFERENCES `pick_up` (`rent_ID`);
-
---
 -- Constraints for table `pick_up`
 --
 ALTER TABLE `pick_up`
-  ADD CONSTRAINT `pick_up_ibfk_1` FOREIGN KEY (`car_ID`) REFERENCES `car` (`car_ID`);
+  ADD CONSTRAINT `pick_up_ibfk_1` FOREIGN KEY (`fk_customer_ID`) REFERENCES `customer` (`customer_ID`),
+  ADD CONSTRAINT `pick_up_ibfk_2` FOREIGN KEY (`FK_car_ID`) REFERENCES `car` (`car_ID`);
 
 --
 -- Constraints for table `return`
 --
 ALTER TABLE `return`
-  ADD CONSTRAINT `return_ibfk_1` FOREIGN KEY (`customer_ID`) REFERENCES `customer` (`customer_ID`);
+  ADD CONSTRAINT `return_ibfk_1` FOREIGN KEY (`fk_customer_ID`) REFERENCES `customer` (`customer_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
